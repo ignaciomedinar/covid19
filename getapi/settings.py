de144b32340511c5f1572a46b1029fc8a01f0aae
Ignaciomedinar <ignaciomedinar@gmail.com>
2021-04-08 14:03:52 -0500

    heroku prueba agregando logging

--- getapi/settings.py
+++ getapi/settings.py
@@ -137,3 +137,19 @@ STATICFILES_DIRS = (
 )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
